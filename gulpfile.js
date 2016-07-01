f100993d203c8fbc9d6d5125e5d8690144cad5d3
Brian Simoni <simonibc@vcu.edu>
2016-06-30 21:27:16 -0400

    Add gulpfile and refactor to .SCSS

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+/**
+ * Created by brian on 6/30/16.
+ */
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('./wp-content/themes/illdy-child/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./wp-content/themes/illdy-child/styles'));
+});
